1478af4a39341690fa1e1a230d424dd4df1be638
eyes-off <eugenecitfoia@gmail.com>
2025-11-17 16:48:55 -0800

    Config updates

--- PublicRecords/EPD-2025-2981_GrantContractCommunication/email-browser/vite.config.js
+++ PublicRecords/EPD-2025-2981_GrantContractCommunication/email-browser/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/eugene-emails/',
+  base: '/eugene-oregon/PublicRecords/EPD-2025-2981_GrantContractCommunication/email-browser/',
 })
