977c96dc9a9183c9c109e4dcf29bcaa2770ae92c
eyes-off <eugenecitfoia@gmail.com>
2025-11-17 17:02:04 -0800

    Removed browsable emails until I figure out how to make them work.

--- PublicRecords/EPD-2025-2981_GrantContractCommunication/email-browser/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/eugene-oregon/PublicRecords/EPD-2025-2981_GrantContractCommunication/email-browser/',
-})
